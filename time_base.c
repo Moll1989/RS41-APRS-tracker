4433ead71524309857ba7d1e7a8fa6ea2b09f545
Moll1989 <colin.moll@gmail.com>
2021-07-22 13:22:24 +0930

    Remove irrelevant example code

--- time_base.c
+++ time_base.c
@@ -1,4 +1,5 @@
 #include "time_base.h"
+
  Void TIME_NVIC_Configuration(void)//If the system generates multiple interrupts, then there is a priority for the interrupt response
 {
     NVIC_InitTypeDef NVIC_InitStructure;
@@ -19,22 +20,4 @@
     TIM_TimeBaseInit(TIM6, &TIM_TimeBaseStructure); 
          TIM_ITConfig(TIM6, TIM_IT_Update|TIM_IT_Trigger, ENABLE); / / enable or disable TIM interrupt, see Figure 1 in the appendix
     TIM_Cmd(TIM6, ENABLE);  
-}
- 
- Void GPIO_Config(void)//Configure an IO port:
-{		
-         /*Define a structure of type GPIO_InitTypeDef*/
-     GPIO_InitTypeDef GPIO_InitStructure;
-         /*Turn on the peripheral clock of GPIOB*/
-     RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
-         /*Select the GPIOB pin to be controlled*/	   
-     GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;	
-         /*Set the pin mode to universal push-pull output*/
-     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;   
-         /* set the pin rate to 50MHz */   
-     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz; 
-         /* Call the library function to initialize GPIOB*/
-     GPIO_Init(GPIOB, &GPIO_InitStructure);
-         /*PB.0 output high*/
-     GPIO_SetBits(GPIOB,GPIO_Pin_0);   
 }
