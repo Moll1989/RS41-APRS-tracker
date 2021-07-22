7d0eff13cd5132df95d08c5c712d32cddcee9364
Moll1989 <colin.moll@gmail.com>
2021-07-22 13:00:15 +0930

    Create time_base.h

---
+++ time_base.h
@@ -0,0 +1,7 @@
+#ifndef __TIMER_BASE_H
+#define __TIMER_BASE_H
+#include "stm32f10x.h"
+    void TIME_NVIC_Configuration(void);
+    void TIME_Configuration(void);
+    void GPIO_Config(void);
+#endif
